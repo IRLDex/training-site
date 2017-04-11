8ef15eb0121f7eba2576a1fd2f14486ca58fdc52
Travis <travis@magnetar.at.local>
2017-04-11 10:39:35 -0600

    JAVASCRIPT: setup javascript file and jquery library

---
+++ scripts.js
@@ -0,0 +1,8 @@
+
+// document.getElementById('avatar').addEventListener('click', function(){
+//   alert('hello from a;lkjasd;fj');
+// });
+
+$("#avatar").on('mouseover', function(){
+  $("#actualname").toggle();
+});
