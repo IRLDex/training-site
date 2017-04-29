6e59ab3f8089d2a2b673ff968f0203714ef85ca1
Travis <travis@Megatrons-MacBook-Pro-8.local>
2017-04-28 23:05:52 -0600

    STYLING: playing with Flex boxes, actual content commented out

--- scripts.js
+++
@@ -1,8 +0,0 @@
-
-// document.getElementById('avatar').addEventListener('click', function(){
-//   alert('hello from a;lkjasd;fj');
-// });
-
-// $("#avatar").on('mouseover', function(){
-//   $("#actualname").toggle();
-// });
